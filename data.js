69029a7c7b812c006af48708906e7ab0446a9d52
ynakazab664 <81090995+ynakazab664@users.noreply.github.com>
2021-08-21 12:37:12 +0900

    Add files via upload

---
+++ data.js
@@ -0,0 +1,8 @@
+﻿function articleArray(){
+const articles=[
+{title:"テスト記事１",content:"てすとてすとてすとてすとてすと。<strong>てすと</strong>てすとてすお",start:"2021/08/21",end:"2021/08/23"},
+{title:"テスト記事２",content:"てててて。",start:"2021/08/20",end:"2021/08/20"},
+{title:"テスト記事３",content:"てすとてすとてすと<img src='images/001.png'>",start:"2021/08/20",end:"2021/08/22"},
+];
+return articles;
+}
